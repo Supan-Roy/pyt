cf7aea16390d1d85775251abf46f2ef976d911ce
Supan Roy <supanroy2021@gmail.com>
2025-05-19 00:13:40 +0600

    SR commit

---
+++ Chapter 4/01_list.py
@@ -0,0 +1,8 @@
+myList = ["Apple", "Orange", 5, 451.21, False, "Supan", "Roy"]
+
+print(myList[0])
+myList[0] = "Grapes" # Grapes replace Apple
+                     # Unlike strings, lists are mutable
+
+print(myList[0])
+print(myList) # Shows updated list
