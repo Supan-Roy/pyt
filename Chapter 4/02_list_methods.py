b4358415b8ed79523abbe45e39401b053bbea6d8
Supan Roy <supanroy2021@gmail.com>
2025-05-31 21:39:19 +0600

    SR commits

---
+++ Chapter 4/02_list_methods.py
@@ -0,0 +1,17 @@
+myList = ["Apple", "Orange", 5, 451.21, False, "Supan", "Roy"]
+print(myList)
+
+myList.append("Haris")  #Adding an element in list at the end
+print(myList)
+
+l1 = [2, 12, 25, 9, 34, 3]
+l1.sort()  #Sorting a list
+print(l1)
+l1.reverse() #Reversing the list
+print(l1)
+
+l1.insert(3, 43) #Inserting 43 in the index 3
+print(l1)
+
+l1.pop(3) #Popping out 3th indexed value
+print(l1)
