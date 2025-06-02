7e2de95533474a5909d76c0e6daaff8bb55b0ba6
Supan Roy <supanroy2021@gmail.com>
2025-06-02 13:09:16 +0600

    edited

--- Chapter 5/Problem4.py
+++ Chapter 5/Problem4.py
@@ -1,7 +1,8 @@
+# What will be the length of following set s
 s = set()
 s.add(14)
 s.add(14.0)
 s.add('14') # length of s after these operations?
 
-print(s)  #Treats 14 (int) and 14.0 (float) as same
-# print(len(s))
+#print(s)  #Treats 14 (int) and 14.0 (float) as same
+print(len(s))
