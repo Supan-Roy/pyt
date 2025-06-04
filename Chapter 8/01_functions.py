bbd2870d1090146a47280faa7f622534417050ac
Supan Roy <supanroy2021@gmail.com>
2025-06-04 16:24:27 +0600

    Initial commit

---
+++ Chapter 8/01_functions.py
@@ -0,0 +1,10 @@
+def avg():
+        a = int(input("Enter a number: "))
+        b = int(input("Enter another number: "))
+        c = int(input("Enter another number: "))
+
+        average = (a+b+c)/3
+        print(average)
+
+avg() #Function called
+print("Thank You!")
---
+++ Chapter 8/02_function_with_arguments.py
@@ -0,0 +1,8 @@
+def goodDay(name, ending):
+        print("Good Day "+ name)
+        print(ending)
+        return "Welcome!"
+
+goodDay("Supan", "Thank you")
+a = goodDay("Jenny", "Thanks")
+print(a)
